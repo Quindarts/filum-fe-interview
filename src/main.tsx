29a89c57c0c3d2facb2495903f5079adef9ab087
Quindarts <quang82thcspb@gmail.com>
2024-11-08 16:56:06 +0700

    new: logo Filum

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider, Zoom } from '@mui/material';
 import theme from './Theme';
 import { SnackbarProvider } from 'notistack';
